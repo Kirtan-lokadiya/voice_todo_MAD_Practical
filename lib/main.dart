eea1bd2d571dd445b84a5422043604f6be0f925f
Kirtan Lokadiya <kirtanlokadiya998@gmail.com>
2025-04-30 14:17:24 +0530

    Speak to live trascribe is Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() {
   runApp(const MyApp());
@@ -20,19 +21,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _transcription = 'Press the mic and start speaking';
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  Future<void> _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (status) => print('Status: $status'),
+        onError: (error) => print('Error: $error'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (result) {
+            setState(() {
+              _transcription = result.recognizedWords;
+            });
+          },
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Voice ToDo')),
-      body: const Center(child: Text('Click mic to start speaking...')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          _transcription,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // We'll add voice input here in next step
-        },
-        child: const Icon(Icons.mic),
+        onPressed: _listen,
+        child: Icon(_isListening ? Icons.mic_off : Icons.mic),
       ),
     );
   }
